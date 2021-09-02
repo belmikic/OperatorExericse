65035854499f1000712f8b9893bf46effa47c0e8
belmikic <belmikic@gmail.com>
2021-09-02 15:27:38 -0700

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var quotient = a / b;
+            var remainder = a % b;
+
+            if (a == 17 && b == 4)
+
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder} ");
+
+            else
+                Console.WriteLine($"{a} / {b} is not {quotient} remainder {remainder}");
+
+        }
+
+
+         
+
+    }
+}
+    
+
